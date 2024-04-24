1face6512c0bb4524619ef9c169e57b5b7991470
denningcars <denningcars@gmail.com>
2024-04-23 19:33:30 -0500

    updated

--- Develop/script.js
+++ Develop/script.js
@@ -4,16 +4,51 @@ const addEmployeesBtn = document.querySelector('#add-employees-btn');
 // Collect employee data
 const collectEmployees = function() {
   // TODO: Get user input to create and return an array of employee objects
+  const employees = []
+  
+  let letsContinueGoing = true
+
+  while(letsContinueGoing) {
+    const firstName = prompt("Enter the employee's first name:");
+    const lastName = prompt("Enter the employee's last name:");
+    let salary = prompt("Enter the employee's salary:");
+
+    if(salary === null || salary === "" || isNaN(salary)){
+      salary = 0;
+    }
+
+    const employee = {
+      firstName: firstName,
+      lastName: lastName,
+      salary: parseInt(salary)
+    }
+
+    employees.push(employee);
+
+    letsContinueGoing = confirm("Do you want to add another employee?");
+  }
+
+  return employees;
 }
 
 // Display the average salary
 const displayAverageSalary = function(employeesArray) {
   // TODO: Calculate and display the average salary
+  let totalSalary = 0;
+  let numEmployees = employeesArray.length;
+  for(let index = 0; index < numEmployees; index++){
+    totalSalary += employeesArray[index].salary;
+  }
+
+  const averageSalary = totalSalary / numEmployees;
+  console.log(`The average employee salary between our ${numEmployees} employee(s) is $${averageSalary.toFixed(2)}`);
 }
 
 // Select a random employee
 const getRandomEmployee = function(employeesArray) {
   // TODO: Select and display a random employee
+  const RandomEmployee = employeesArray[Math.floor(Math.random() * employeesArray.length)]
+  console.log(`Random Employee: ${RandomEmployee["firstName"]} ${RandomEmployee["lastName"]} - is our daily winner!`);
 }
 
 /*
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # module-03-challenge
+for this challenge I wanted to build an application to add employees and their salaries
